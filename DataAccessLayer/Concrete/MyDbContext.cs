f67c1fc47ae697c4e1544236d30336f3f00fae8d
Rafet Karaca <karacarafet32@gmail.com>
2023-03-30 14:00:30 +0300

    DataAccess Katmanı

--- DataAccessLayer/Concrete/MyDbContext.cs
+++ DataAccessLayer/Concrete/MyDbContext.cs
@@ -10,6 +10,9 @@ namespace DataAccessLayer.Concrete
 {
     public class MyDbContext: DbContext
     {
+        public MyDbContext()
+        {
+        }
         public MyDbContext(DbContextOptions<MyDbContext> options)
         : base(options)
         { }
