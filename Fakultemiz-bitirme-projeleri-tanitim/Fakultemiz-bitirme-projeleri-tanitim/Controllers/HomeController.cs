b669ce40409aa98b2a0f89daf3b629cdc7dbd16e
Rafet Karaca <karacarafet32@gmail.com>
2023-03-30 14:52:43 +0300

    Anasayfa Template eklenmesi

--- Fakultemiz-bitirme-projeleri-tanitim/Fakultemiz-bitirme-projeleri-tanitim/Controllers/HomeController.cs
+++ Fakultemiz-bitirme-projeleri-tanitim/Fakultemiz-bitirme-projeleri-tanitim/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace Fakultemiz_bitirme_projeleri_tanitim.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
