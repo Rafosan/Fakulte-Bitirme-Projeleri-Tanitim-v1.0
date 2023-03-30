8e9c3061d235b9cd5843c3e65e5719caa63678c1
Rafet Karaca <karacarafet32@gmail.com>
2023-03-30 11:17:23 +0300

    Add project files.

---
+++ Fakultemiz-bitirme-projeleri-tanitim/Fakultemiz-bitirme-projeleri-tanitim/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fakultemiz_bitirme_projeleri_tanitim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
