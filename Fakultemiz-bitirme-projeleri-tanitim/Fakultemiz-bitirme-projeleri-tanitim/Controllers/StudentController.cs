3483059625d80beb009a43f6e8bac46f09a0ab38
Rafet Karaca <karacarafet32@gmail.com>
2023-04-03 23:25:22 +0300

    Öğrenci controller

---
+++ Fakultemiz-bitirme-projeleri-tanitim/Fakultemiz-bitirme-projeleri-tanitim/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fakultemiz_bitirme_projeleri_tanitim.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentManager studentManager=new StudentManager(new EfStudentDal());
+        public IActionResult Index()
+        {
+            var values = studentManager.TGetAll();
+            return View(values);
+        }
+    }
+}
