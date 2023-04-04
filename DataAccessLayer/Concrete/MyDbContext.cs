3b096db08465b98aa1e02352248a2876135e5395
Rafet Karaca <Rafet Karaca@RAFETKARACA-PC>
2023-04-04 21:14:45 +0300

    Deneme

--- DataAccessLayer/Concrete/MyDbContext.cs
+++ DataAccessLayer/Concrete/MyDbContext.cs
@@ -10,9 +10,6 @@ namespace DataAccessLayer.Concrete
 {
     public class MyDbContext: DbContext
     {
-        public MyDbContext()
-        {
-        }
         public MyDbContext(DbContextOptions<MyDbContext> options)
         : base(options)
         { }
