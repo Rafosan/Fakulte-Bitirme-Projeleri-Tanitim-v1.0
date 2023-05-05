dfb7580e5cfe9681d83b9fd3b3842ba8b66f203a
Rafet Karaca <karacarafet32@gmail.com>
2023-05-05 10:38:24 +0300

    About silme ve Controller ekleme

---
+++ Fakultemiz-bitirme-projeleri-tanitim/Fakultemiz-bitirme-projeleri-tanitim/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fakultemiz_bitirme_projeleri_tanitim.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
